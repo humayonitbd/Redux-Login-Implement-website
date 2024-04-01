e48f4bc8d64dc373a535c2e36a1dbe6c1601e44d
humayonitbd <humayonforid44@gmail.com>
2024-04-01 15:08:46 +0600

    redux async work

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,4 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit/dist';
+import {createUserWithEmailAndPassword} from "firebase/auth"
+import auth from '../../../utils/firebase.config';
 
 const initialState = {
   name: '',
@@ -8,10 +11,24 @@ const initialState = {
   error:"",
 };
 
+
+export const createUser = createAsyncThunk("userSlice/createUser",async({email,password})=>{
+ const data = await createUserWithEmailAndPassword(auth,email,password);
+ console.log(data);
+ return;
+});
+
+
+
 const userSlice = createSlice({
   name: 'userSlice',
   initialState,
   reducers: {},
+  extraReducers:(builder)=>{
+    builder.addCase(createUser.pending,(state,{payload})=>{
+      
+    });
+  }
 });
 
 export default userSlice.reducer;
